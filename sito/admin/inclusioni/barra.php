09b4158a93b7524367ec210cc55872ea4eadc960
ionuttttt <145325230+ionuttttt@users.noreply.github.com>
2025-05-20 10:03:18 +0200

    inclusioni

---
+++ sito/admin/inclusioni/barra.php
@@ -0,0 +1,36 @@
+	<nav class="ts-sidebar">
+			<ul class="ts-sidebar-menu">
+			
+				<li class="ts-label">Main</li>
+				<li><a href="dashboard.php"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+			
+<li><a href="#"><i class="fa fa-files-o"></i> Marchi</a>
+<ul>
+<li><a href="crea-marchio.php">Crea Marchio</a></li>
+<li><a href="gestisci-marchi.php">Gestisci Marchi</a></li>
+</ul>
+</li>
+
+<li><a href="#"><i class="fa fa-car"></i> Veicoli</a>
+					<ul>
+						<li><a href="crea-veicolo.php">Pubblica un Veicolo</a></li>
+						<li><a href="gestisci-veicoli.php">Gestisci Veicoli</a></li>
+					</ul>
+				</li>
+
+<li><a href="#"><i class="fa fa-sitemap"></i> Noleggi</a>
+					<ul>
+						<li><a href="nuovi-noleggi.php">Nuovi</a></li>
+						<li><a href="noleggi-confermati.php">Confermati</a></li>
+						<li><a href="noleggi-cancellati.php">Cancellati</a></li>
+					</ul>
+				</li>
+
+		
+
+				<li><a href="recensioni.php"><i class="fa fa-table"></i> Gestisci Recensioni</a></li>
+				<li><a href="gestisci-richieste.php"><i class="fa fa-desktop"></i> Gestisci Richieste degli Utenti</a></li>
+				<li><a href="reg-users.php"><i class="fa fa-users"></i> Utenti Registrati</a></li>
+
+			</ul>
+		</nav>
